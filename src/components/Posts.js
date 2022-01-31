790d5cfe070a6ac434220a16e906bdc96f977897
James Spencer <jameskcs1254@gmail.com>
2022-01-31 14:12:01 +0000

    Implement posts component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react/cjs/react.development";
+
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('http://localhost:8000/api/post', {mode:'cors'});
+      const posts = await response.json();
+  
+      setPosts(posts);
+    }
+    fetchPosts();
+  },[])
+
   return (
     <div className="Posts">
-      
+      <h1>Posts</h1>
+       {posts.map(post => {
+         return(
+            <Link key={post._id} to={'/posts/' + post._id}>
+              <h2>{post.title}</h2>
+              <p>{post.text}</p>
+            </Link>
+          )
+       })}
     </div>
   );
 }
