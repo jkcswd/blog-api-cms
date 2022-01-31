dc01150c1956d47d6367cb52a374d02b524a44fb
James Spencer <jameskcs1254@gmail.com>
2022-01-31 14:12:17 +0000

    Add links to create and view posts

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const MainPage = () => {
   return (
     <div className="MainPage">
-      
+      <h1>You are in!</h1>
+      <Link to="/posts">View posts</Link>
+      <Link to="/create-post">Create post</Link>
     </div>
   );
 }
