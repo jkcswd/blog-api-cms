284816515c58e2b4310c69b8d8fedd948c87654f
James Spencer <jameskcs1254@gmail.com>
2022-01-31 11:42:39 +0000

    Add routing, conditonal rendering based on JWT.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreatePost from "./components/CreatePost";
+import Login from "./components/Login";
+import MainPage from "./components/MainPage";
+import Post from "./components/Post";
+import Posts from "./components/Posts";
+
 const App = () => {
+  const token = localStorage.getItem('accessToken')
+
+  if(!token) {
+    return <Login/>
+  }
+
   return (
-    <div></div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage/>}/>
+      <Route path="/posts" element={<Posts/>}/>
+      <Route path="/posts/create" element={<CreatePost/>}/>
+      <Route path="/posts/:id" element={<Post/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
