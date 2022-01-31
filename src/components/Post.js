efe60d9f8c51a56d78d73e20d3971338c0a95c47
James Spencer <jameskcs1254@gmail.com>
2022-01-31 14:12:49 +0000

    Implement Single post viewong component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,27 @@
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react/cjs/react.development";
+
 const Post = () => {
+  const location = useLocation();
+  const id = location.pathname.split('/')[2]
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`http://localhost:8000/api/post/${id}`, {mode:'cors'});
+      const post = await response.json();
+  
+      setPost(post);
+    }
+    fetchPost();
+  },[id]);
+
   return (
     <div className="Post">
-      
+      <h1>{post.title}</h1>
+      <p>{post.text}</p>
+      <p>{post.isPublished && 'PUBLISHED'}</p>
+      <p>{!post.isPublished && 'UNPUBLISHED'}</p>
     </div>
   );
 }
