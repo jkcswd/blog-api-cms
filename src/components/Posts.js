bd0c6f06501e5222c2e5565abd7b51fad5d47b13
James Spencer <jameskcs1254@gmail.com>
2022-02-01 12:20:34 +0000

    create button to toggle publish in client/server

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react/cjs/react.development";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
-
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch('http://localhost:8000/api/post', {mode:'cors'});
@@ -14,15 +13,52 @@ const Posts = () => {
     fetchPosts();
   },[])
 
+  const [postToUpdate, setPostToUpdate] = useState(false)
+  useEffect(() => {
+    if(postToUpdate) {
+      const updatePostsByApi = async () => {
+        await fetch(`http://localhost:8000/api/post/${postToUpdate.id}`, {
+          method: 'PATCH',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.accessToken
+          },
+          body: JSON.stringify({ isPublished:postToUpdate.isPublished }
+          )
+        });
+      }
+      updatePostsByApi();
+    }  
+  }, [postToUpdate]);
+
+  const handlePublish = (e) => {
+    const newPosts = posts.map(post => {
+      if(post._id === e.target.value){
+        setPostToUpdate({ id: e.target.value, isPublished: !post.isPublished });
+        return { ...post, isPublished: !post.isPublished };
+      }
+
+      return post;
+    });
+
+    setPosts(newPosts);
+  }
+
   return (
     <div className="Posts">
       <h1>Posts</h1>
        {posts.map(post => {
          return(
-            <Link key={post._id} to={'/posts/' + post._id}>
-              <h2>{post.title}</h2>
-              <p>{post.text}</p>
-            </Link>
+            <div key={post._id}>
+              <Link  to={'/posts/' + post._id}>
+                <h2>{post.title}</h2>
+                <p>{post.text}</p>
+                <p>{post.isPublished && 'Published'}</p>
+                <p>{!post.isPublished && 'Unpublished'}</p>
+              </Link>
+              <button value={post._id} onClick={handlePublish}>Publish/Unpublish</button>
+            </div>
           )
        })}
     </div>
