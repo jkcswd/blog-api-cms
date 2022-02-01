2a154d4212d426ba10363aa9b0e37b1b122a79f6
James Spencer <jameskcs1254@gmail.com>
2022-02-01 10:39:14 +0000

    Fetch and display comments

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react/cjs/react.development";
 const Post = () => {
   const location = useLocation();
   const id = location.pathname.split('/')[2]
-  const [post, setPost] = useState({});
+  const [post, setPost] = useState(false);
+  const [comments, setComments] = useState([])
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -14,6 +15,14 @@ const Post = () => {
       setPost(post);
     }
     fetchPost();
+
+    const fetchComments = async () => {
+      const response = await fetch(`http://localhost:8000/api/post/${id}/comments`, {mode:'cors'});
+      const comments = await response.json();
+  
+      setComments(comments);
+    }
+    fetchComments();
   },[id]);
 
   return (
@@ -22,6 +31,18 @@ const Post = () => {
       <p>{post.text}</p>
       <p>{post.isPublished && 'PUBLISHED'}</p>
       <p>{!post.isPublished && 'UNPUBLISHED'}</p>
+      <h2>Comments</h2>
+      {comments.map(comment => {
+        return(
+          <div key={comment._id}>
+            <p>{comment.comment}</p>
+            <p>{comment.user.username}</p>
+            <p>{comment.datePublished}</p>
+          </div>
+        )
+      })
+
+      }
     </div>
   );
 }
