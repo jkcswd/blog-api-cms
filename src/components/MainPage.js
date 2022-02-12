4df23ac60ad334b2e840cc8d52a238af4fe97651
James Spencer <jameskcs1254@gmail.com>
2022-02-12 14:14:03 +0000

    Add divs for styling, import css

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import '../styles/MainPage.css'
 
 const MainPage = () => {
   return (
     <div className="MainPage">
       <Header/>
-      <Link to="/posts">View posts</Link>
-      <Link to="/create-post">Create post</Link>
+      <div className="main-page-content">
+        <Link to="/posts">
+          <div className="link-container">View posts</div>
+        </Link>
+        <Link to="/create-post">
+          <div className="link-container">Create post</div>
+        </Link>
+      </div>
       <Footer/>
     </div>
   );
